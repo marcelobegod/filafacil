817bc399d8e2e8527fe7c9b7040e6356a5f344d0
Marcelo Ferreira de Oliveira <69762904+marcelobegod@users.noreply.github.com>
2024-06-08 20:59:49 -0300

    botões pagina inicial

--- home.php
+++ home.php
@@ -10,12 +10,36 @@
 <body>
 <main>
     <div class="container">
-            <div class="filafacil">
-                <p>Fila Fácil</p>
+        <div class="filafacil">
+            <p>Fila Fácil</p>
+        </div>
+        <div class="navegador">
+            <p>Página inicial</p>
+        </div>
+        <div class="container">
+            <div class="conteudo">
+                <div class="row ">
+                    <div class="text-center alig mb-4">
+                        <a href="#" class="btn btn-primary largurabtn textobtn alig">Criar fila</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="text-center mb-4">
+                        <a href="#" class="btn btn-primary largurabtn textobtn">Entrar na fila</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="text-center mb-4">
+                        <a href="#" class="btn btn-primary largurabtn textobtn">Listar filas</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="text-center">
+                        <img src="img/logoff.png" class="img-fluid" alt="Logoff">
+                    </div>
+                </div>
             </div>
-            <div class="navegador">
-                <p>Página inicial</p>
-            </div>
+        </div>
     </div>   
 </main>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
