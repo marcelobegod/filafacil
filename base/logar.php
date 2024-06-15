06a88756e5e3ac772dc645782123267713bcd0a4
Marcelo Ferreira de Oliveira <69762904+marcelobegod@users.noreply.github.com>
2024-06-15 15:16:30 -0300

    botão home na area de login

--- base/logar.php
+++ base/logar.php
@@ -6,6 +6,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/style.css">
     <title>Fila Fácil</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
 </head>
 <body>
 <main>
@@ -19,8 +21,8 @@
         <div class="container">
             <div class="conteudo text-center"> <!-- Adicionado a classe text-center para centralizar todo o conteúdo -->
                 <div class="row justify-content-center mb-4"> <!-- Adicionado justify-content-center para centralizar horizontalmente -->
-                    <div class="alert alert-danger largbut" role="alert">
-                        Um simples alerta danger. Olha só!
+                    <div class="alert alert-danger largmsg" role="alert">
+                        Mensagem de erro de login vai ser aqui
                     </div>
                     <form action="validacao.php" method="post">
                         <div class="form-group row justify-content-center mb-1">
@@ -40,11 +42,16 @@
                         <a href="#">Criar cadastro</a>
                     </div>
                 </div>
-                <div class="row justify-content-center"> <!-- Adicionado justify-content-center para centralizar horizontalmente -->
-                    <div class="text-center">
+                <div class="row w-100 p-0 m-0"> <!-- Adicionado justify-content-center para centralizar horizontalmente -->
+                    <div class="col-3"></div>
+                    <div class="col-6 text-center">
                         <img src="../img/logoff.png" class="img-fluid" alt="Logoff">
                     </div>
+                    <div class="col-3 d-flex flex-column align-items-start justify-content-end pr-3">
+                        <a href="http://localhost/filafacil/base/home.php" class="btn btn-primary btnhome"><i class="fas fa-home"></i> Home</a>
+                    </div>
                 </div>
+
             </div>
         </div>
     </div>   
