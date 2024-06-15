1965759f09bfb237c74cab5e2d9bb0a7899c84d6
Marcelo Ferreira de Oliveira <69762904+marcelobegod@users.noreply.github.com>
2024-06-15 14:39:45 -0300

    ajuste tela logar e incremento da div msg alerta

--- base/logar.php
+++ base/logar.php
@@ -17,37 +17,39 @@
             <p>Logar</p>
         </div>
         <div class="container">
-    <div class="conteudo text-center"> <!-- Adicionado a classe text-center para centralizar todo o conteúdo -->
-        <div class="row justify-content-center"> <!-- Adicionado justify-content-center para centralizar horizontalmente -->
-                    <form action="base/validacao.php" method="post">
-                        <div class="input-group form-group">             
-                            <input type="text" class="form-control" placeholder="usuario" name="usuario">
+            <div class="conteudo text-center"> <!-- Adicionado a classe text-center para centralizar todo o conteúdo -->
+                <div class="row justify-content-center mb-4"> <!-- Adicionado justify-content-center para centralizar horizontalmente -->
+                    <div class="alert alert-danger largbut" role="alert">
+                        Um simples alerta danger. Olha só!
+                    </div>
+                    <form action="validacao.php" method="post">
+                        <div class="form-group row justify-content-center mb-1">
+                            <input type="text" class="form-control" style="width: 261px; height: 40px;" placeholder="Usuário" name="usu_usu">
                         </div>
-                        <div class="input-group form-group">
-                            <input type="password" class="form-control" placeholder="senha" name="senha">
+                        <div class="form-group row justify-content-center mb-4  ">
+                            <input type="password" class="form-control" style="width: 261px; height: 40px;" placeholder="Senha" name="senha_usu">
                         </div>
-                        <div class="row align-items-center remember">
-                            <input type="checkbox">Lembrar
-                        </div>
-                        <div class="form-group">
-                            <input type="submit" value="Logar" class="btn float-right login_btn">
+                        <div class="justify-content-center mb-4">
+                            <div class="col-12 form-group ">
+                            <input type="submit" value="Logar" class="btn btn-primary largurabtn textobtn ">
+                            </div>
                         </div>
                     </form>
-            <div>
-                <a href="#">Esqueceu a senha?</a> |
-                <a href="#">Criar cadastro</a>
+                    <div>
+                        <a href="#">Esqueceu a senha?</a> |
+                        <a href="#">Criar cadastro</a>
+                    </div>
+                </div>
+                <div class="row justify-content-center"> <!-- Adicionado justify-content-center para centralizar horizontalmente -->
+                    <div class="text-center">
+                        <img src="../img/logoff.png" class="img-fluid" alt="Logoff">
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row justify-content-center"> <!-- Adicionado justify-content-center para centralizar horizontalmente -->
-            <div class="text-center">
-                <img src="../img/logoff.png" class="img-fluid" alt="Logoff">
-            </div>
-        </div>
-    </div>
-</div>
-
     </div>   
 </main>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
