0cc8847dd12cb106bd7a0e921e10e8a357491a9b
Marcelo Ferreira de Oliveira <69762904+marcelobegod@users.noreply.github.com>
2024-06-16 13:05:30 -0300

    ajustes

---
+++ base/navbar2.php
@@ -0,0 +1,11 @@
+<div class="row d-flex justify-content-center mx-auto largfoot">
+    <div class="col-4 p-1">
+        <a href="#" class="btn btn-primary btnhome"><i class="fas fa-list btn-menu"></i>Filas</a>
+    </div>
+    <div class="col-4 p-1">
+        <a href="http://localhost/filafacil/base/home.php" class="btn btn-primary btnhome"><i class="fas fa-home btn-menu"></i> Home</a>
+    </div>
+    <div class="col-4 p-1">
+        <a href="#" class="btn btn-primary btnhome"><i class="fas fa-user btn-menu"></i> Usuário</a>
+    </div>
+</div>
---
+++ base/testanivel.php
@@ -0,0 +1,9 @@
+<?php
+if(!isset($_SESSION)) session_start();
+
+if(!isset($_SESSION['UsuarioID']) OR ($_SESSION['UsuarioNivel'] <= $nivel_necessario)) {
+	session_destroy();
+	header("Location: ../home.php"); 
+	exit;
+}
+?>
