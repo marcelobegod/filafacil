db22b2ddf5aaccc7487c0d321416b82d188ac574
Helyomar Lins <111030700+HelyomarLins@users.noreply.github.com>
2024-06-21 16:45:20 -0300

    Preparando ambiente
    Excluindo pastas extras. Preparando ambiente de desenvolvimento

--- base/testanivel.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!isset($_SESSION)) session_start();
-
-if(!isset($_SESSION['UsuarioID']) OR ($_SESSION['UsuarioNivel'] <= $nivel_necessario)) {
-	session_destroy();
-	header("Location: ../home.php"); 
-	exit;
-}
-?>
